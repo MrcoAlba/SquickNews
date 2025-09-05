bd9459d78adf1fd25ea9c73fc6454643d206e141
Mrco Alba <mrcoalba@gmail.com>
2025-09-05 13:34:25 -0500

    Initial Commit

---
+++ SquickNews/SquickNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquickNewsApp.swift
+//  SquickNews
+//
+//  Created by Marco Antonio Landauro Alba on 5/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SquickNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
